e8f3b97496c1bdeafff274133652ef294842e5ba
tdev30 <tibodevos30@gmail.com>
2026-04-28 19:15:29 +0200

    Show stats on selected datapoint

--- infoVis/dashboard.js
+++ infoVis/dashboard.js
@@ -35,6 +35,43 @@ const cluster = L.markerClusterGroup({
     zoomToBoundsOnClick: true,
 });
 
+function getCrimePartLabel(part) {
+  if (part === 1 ) return "Serious Crime";
+  if (part === 2) return "Less Serious Crime";
+  return "Unknown";
+}
+
+function createPopup(d) {
+  return `
+    <b>Main Crime:</b> ${d["Crm Cd Desc"] || "N/A"} (${d["Crm Cd"] || "-"})<br>
+    <b>Date:</b> ${d["DateTime OCC"] || "N/A"}<br>
+    <b>Status:</b> ${d["Status Desc"] || "N/A"} (${d["Status"] || "-"})<br>
+    <b>Classification:</b> ${getCrimePartLabel(d["Part 1-2"])} (${d["Part 1-2"] || "-"})<br>
+    <br>
+
+    <b>Modus Operandi:</b> ${d["MO Desc"] || "N/A"} (${d["Mocodes"] || "-"})<br>
+    <b>Weapon:</b> ${d["Weapon Desc"] || "N/A"} (${d["Weapon Used Cd"] || "-"})<br>
+    <br>
+    
+    <b>Victim Age:</b> ${d["Vict Age"] ?? "N/A"}<br>
+    <b>Victim Sex:</b> ${d["Vict Sex"] || "N/A"}<br>
+    <b>Victim Descent:</b> ${d["Vict Descent Desc"] || "N/A"} (${d["Vict Descent"] || "-"})<br>
+    <br>
+
+    <b>Area:</b> ${d["AREA NAME"] || "N/A"} (${d["AREA"] || "-"})<br>
+    <b>Location:</b> ${d["LOCATION"] || "N/A"}<br>
+    <b>Premise:</b> ${d["Premis Desc"] || "N/A"} (${d["Premis Cd"] || "-"})<br>
+    <br>
+
+    <b>Additional Crime 2:</b> ${d["Crm Cd 2 Desc"] || "N/A"} (${d["Crm Cd 2"] || "-"})<br>
+    <b>Additional Crime 3:</b> ${d["Crm Cd 3 Desc"] || "N/A"} (${d["Crm Cd 3"] || "-"})<br>
+    <b>Additional Crime 4:</b> ${d["Crm Cd 4 Desc"] || "N/A"} (${d["Crm Cd 4"] || "-"})<br>
+    <br>
+    
+    <b>ID:</b> ${d["DR_NO"] || "-"}
+  `;
+}
+
 
 fetch("/data")
     .then(res => res.json())
@@ -42,7 +79,7 @@ fetch("/data")
         data.forEach(d => {
             if (d.LAT && d.LON) {
                 const marker = L.marker([d.LAT, d.LON])
-                    .bindPopup(`<b>${d["Crm Cd Desc"]}</b><br>${d["DateTime OCC"]}<br>${d["AREA NAME"]}`);
+                    .bindPopup(createPopup(d));
                 cluster.addLayer(marker);
             }
         });
